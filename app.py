e5cfb9d586fec6b2104a456f3eabc4c8d21a298a
Tianle Chen <tianlechen@gmail.com>
2022-03-02 21:19:00 -0500

    NUM_CACHED_PIPELINES

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import functools
+import os
 
 from fastapi import FastAPI, Request
 from transformers import pipeline
 
 app = FastAPI()
 
+NUM_CACHED_PIPELINES = os.getenv('NUM_CACHED_PIPELINES', 1)
 
-@functools.lru_cache(maxsize=1)
+
+@functools.lru_cache(maxsize=NUM_CACHED_PIPELINES)
 def get_pipeline(**kwargs):
     return pipeline(**kwargs)
 
